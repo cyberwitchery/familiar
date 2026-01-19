7948abaa558a2f34542b6f14c11975e818edf85f
Veit Heller <veit@veitheller.de>
2026-01-19 15:25:17 +0100

    remove version fallback

--- src/familiar/__init__.py
+++ src/familiar/__init__.py
@@ -4,7 +4,4 @@ from importlib.metadata import version, PackageNotFoundError
 
 __all__ = ["agents", "render", "cli"]
 
-try:
-    __version__ = version("familiar-cli")
-except PackageNotFoundError:
-    __version__ = "0.0.0-dev"
+__version__ = version("familiar-cli")
